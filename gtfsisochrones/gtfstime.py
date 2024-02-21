6b046c00c9a43f659dac4363c8e891d64a914596
Fabio Mazza <fab4mazz@gmail.com>
2024-02-21 18:08:18 +0100

    add license

--- gtfsisochrones/gtfstime.py
+++ gtfsisochrones/gtfstime.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import polars as pl
 
 map_days={i:k for i,k in enumerate(("monday","tuesday","wednesday","thursday","friday","saturday","sunday"))}
